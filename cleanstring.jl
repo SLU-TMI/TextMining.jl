1e9cef6df8e2c9d775a618caa41cc74d60d9717d
ljekersey <kersey@slu.edu>
2015-01-08 16:19:55 -0600

    function to remove punctuation and uppercase from strings

--- cleanstring.jl
+++ cleanstring.jl
@@ -1,3 +1,15 @@
-function clean(args)
-	#function body
+punctuation = ['!','"','#','$','%','&','\'','(',')','*','+',',',
+'-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`',
+'{','|','}','~']
+
+function clean(string)
+	string = lowercase(string)
+	sarray = split(string)
+	x = 1 
+	while x <= length(sarray)
+		sarray[x] = strip(sarray[x], punctuation)
+		x += 1
+	end
+	string = join(sarray, ' ')
+	return string
 end
