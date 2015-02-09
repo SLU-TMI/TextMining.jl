d29b54846cd3851f9a4b5c42bc36fe92bacce728
Kevin Damazyn <damazynk@slu.edu>
2015-02-09 16:42:15 -0600

    commiting method to find centroids and a method to interate over each centroid. -kd

--- src/clustering.jl
+++ src/clustering.jl
@@ -8,56 +8,7 @@ function kmeans(k::Integer, clust::Dict, max_iter::Integer) = kmeans(k,clust,cos
 function kmeans(k::Integer, clust::Dict, dist_func::Function) = kmeans(k,clust,dist_func,10000)
 
 function kmeans(k::Integer, clust::Dict, dist_func::Function, max_iter::Integer)
-	centroids = find_centroids(k,clust,dist_func)
-
-	# array of centroids,features
-	for centroid in centroids
-		new_clusters = vcat(Cluster(Dict(),centroid))
-	end
-	new_clusters = find_distances(new_clusters, dist_func, features)
-	return new_clusters
-end 
-
-function find_distances(clusters::Array,dist_func::Function,features)
-	iteration = 1
-	# noChange = false
-	# while (!noChange && iteration < max_iter)
-		# for fv in features
-			# dist = Inf
-			# for cluster in clusters
-				# current_dist = dist_func(cluster.centroid,fv)
-				# if current_dist < dist
-					# dist = current_dist
-					# cluster = cluster
-				# end
-			# end
-			# cluster.clust = Dict(vcat(collect(values(cluster.clust)),fv))
-		# end
-
-		# recompute
-
-		# check if difference
-
-		# if not done reset clusters
-
-
-		iteration++
-	# end
-
-
-	for fv in features
-		min_dist = 0
-		for centroid in centroids
-			dist = dist_func(centroid,fv)
-			if dist < min_dist
-				current_centroid = centroid
-
-	end
-
-end
-
-
-function find_centroids(k::Integer, clust::Dict, dist_func::Function)
+	# find initial centroids
 	rand_num = abs(rand(Int64)%Base.length(clust) + 1)
 	features = values(clust)
 	orig_cent = collect(features)[rand_num]
@@ -68,9 +19,7 @@ function find_centroids(k::Integer, clust::Dict, dist_func::Function)
 		for fv in features
 			current_min_max_dist = Inf
 			for centroid in centroids
-				if fv.map == centroid.map
-					continue
-				else
+				if fv.map != centroid.map
 					min_max_dist = dist_func(centroid,fv)
 					if min_max_dist < current_min_max_dist
 						current_min_max_dist = min_max_dist
@@ -83,85 +32,66 @@ function find_centroids(k::Integer, clust::Dict, dist_func::Function)
 			end
 		end
 		centroids = vcat(centroids,nextCent)
-		numCent = randomNumber
 		cents_to_be_found--;
 	end
-	return centroids
-end
+
+	# make Array of clusters
+	for centroid in centroids
+		new_clusters = vcat(new_clusters, Cluster())
+	end
+
+	# find distance between fv and centroid
+	iteration = 1
+	no_change = false
+	while (!no_change) && (iteration < max_iter)
+		i = 1
+		for fv in features
+			dist = Inf
+			j = 1
+			for centroid in centroids
+				current_dist = dist_func(centroid,fv)
+				if current_dist < dist
+					dist = current_dist
+					min_dist_cluster = new_clusters[j]
+				end
+			end
+			min_dist_cluster[i] = fv
+			i++
+		end
+
+		# recompute new clusters
+		old_centroids = centroids
+		i = 1
+		new_centroids = vcat()
+		for centroid in centroids
+			cluster = new_clusters[i]
+			new_centroids = vcat(new_centroids, cluster.centroid)
+		end
+		# set the new centroids
+		centroids = new_centroids
+
+		# checking if centroids moved.
+		no_change = true
+		for i = 0; i < k; i++
+			dist = dist_func(old_centroids[i],new_centroids[i])
+			if dist > .000001
+				no_change = false
+			end
+		end
+
+		# reset clusters if there are no changes.
+		if (!no_change)
+			new_clusters = vcat()
+			for centroid in centroids
+				new_clusters = vcat(new_clusters, Cluster())
+			end
+		end
+		iteration++
+	end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# function find_sec_cent(centroid, dist_func::Function, features)
-# 	dist = 0
-# 	for fv in features
-# 		max_dist = dist
-# 		dist = dist_func(centroid,fv)
-# 		if dist > max_dist
-# 			max_dist = dist
-# 			sec_cent = fv
-# 		end
-# 	end
-# 	return vcat(centroid,sec_cent)
-# end
-
-# function find_next_cent(centroids, dist_func::Function, features)
-# 	max_min_dist = 0
-# 	for fv in features
-# 		min_dist = Inf
-# 		for centroid in centroids
-# 			dist = dist_func(centroid,fv)
-# 			if dist < min_dist
-# 				min_dist = dist
-# 				if min_dist > max_min_dist
-# 					max_min_dist = min_dist
-# 					next_cent = fv
-# 				end
-# 			end
-# 		end
-# 	end
-# 	return vcat(centroids,next_cent)
-# end
-
+	return new_clusters
+end 
 
 
 
