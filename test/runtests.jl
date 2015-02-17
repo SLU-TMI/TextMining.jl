4c62c2a61ebbc45e159f62a0cab9446741fd336a
Mark Tabor <mtabor3@slu.edu>
2015-02-17 16:50:18 -0600

    added DataSet tests -mt

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,12 @@ include("../src/feature_vector.jl")
 include("../src/distribution.jl")
 include("../src/cluster.jl")
 include("../src/clustering.jl")
+include("../src/data_set.jl")
 
 function run_tests()
   reload("./test/feature_vector_tests.jl")
   reload("./test/distribution_tests.jl")
   reload("./test/cluster_tests.jl")
   reload("./test/clustering_tests.jl")
+  reload("./test/data_set_tests.jl")
 end
