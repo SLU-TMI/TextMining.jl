30ef79a65591a609e2c69170ae5c43878eb718b5
Mark Tabor <mtabor3@slu.edu>
2015-02-18 16:51:32 -0600

    moved smoothing function into distribution -mt

--- src/distribution.jl
+++ src/distribution.jl
@@ -21,11 +21,11 @@ function getindex(d::Distribution, key)
 end
 
 function keys(d::Distribution)
-  return Base.keys(d.fv.map)
+  return keys(d.fv)
 end
 
 function isempty(d::Distribution)
-  return Base.isempty(d.fv.map)
+  return isempty(d.fv)
 end
 
 function entropy(d::Distribution)
@@ -42,4 +42,43 @@ end
 
 function perplexity(d::Distribution)
   return 2^entropy(d)
+end
+
+# really slow add-1 unigram smoothed probability for target key 
+function laplace_smoothed_prob(ds::Distribution, target)
+  unique_keys = 0
+  total_keys = 0
+  ds_fv = FeatureVector() #will be feature vector of entire data set
+  for cluster in ds
+    if !isempty(cluster)
+      ds_fv += cluster.vector_sum
+    end
+  for key in keys(ds_fv)
+    unique_keys += 1
+    total_keys += ds_fv[key]
+  end
+  if !haskey(ds_fv, target)
+    return 1 / (total_keys + unique_keys) 
+  end
+  return (ds_fv[target] + 1) / (total_keys + unique_keys) 
+end
+
+#add-1 smoothing that returns a giant feature vector of the data set with new counts
+function laplace_smoothing(ds::Distribution)
+  unique_keys = 0
+  total_keys = 0
+  ds_fv = FeatureVector() #will be feature vector of entire data set
+  for cluster in ds
+    if !isempty(cluster)
+      ds_fv += cluster.vector_sum
+    end
+  for key in keys(ds_fv)
+    unique_keys += 1
+    total_keys += ds_fv[key]
+  end
+  ds_fv["<UNK>"] = 0       #placeholder for "unknown" key that is not present in data set
+  for key in keys(ds_fv)
+    ds_fv[key] = (ds_fv[key] + 1) * (total_keys / ((total_keys + unique_keys))
+  end 
+  return ds_fv
 end
