83fc039429b3f7a6ec0078dcfd2655265220aa83
Charlie Garavaglia <charlieg895@gmail.com>
2015-02-19 18:36:09 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,8 @@ using FactCheck
 
 include("../src/TextMining.jl")
 
-function run_tests()
-  reload("./test/feature_vector_tests.jl")
-  reload("./test/cluster_tests.jl")
-  reload("./test/data_set_tests.jl")
-  reload("./test/clustering_tests.jl")
-  reload("./test/distribution_tests.jl")
-end
+reload("./test/feature_vector_tests.jl")
+reload("./test/cluster_tests.jl")
+reload("./test/data_set_tests.jl")
+reload("./test/clustering_tests.jl")
+reload("./test/distribution_tests.jl")
